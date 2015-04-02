ffee24012f69e850b775338d353d97ce38fa1192
Jens Breitbart <jbreitbart@gmail.com>
2015-04-02 15:12:25 +0200

    First patch for mask_range_hashing.

--- mask_range_hashing/umap_test.cpp
+++ mask_range_hashing/umap_test.cpp
@@ -4,49 +4,81 @@
 
 #ifdef LESSDIV
 // Specialization of unordered_map to use masking for bucket calculation
-struct _Mod_myrange_hashing {
-	typedef std::size_t first_argument_type;
-	typedef std::size_t second_argument_type;
-	typedef std::size_t result_type;
 
-	static std::size_t mask;
+// Compute mask using in _Mask_range_hashing
+// mask := (smallest power of two greater or equal than __den) - 1
+template <std::size_t SHIFT>
+static inline std::size_t
+_Mask_range_hashing_compute_mask(std::size_t n)
+{
+    n = _Mask_range_hashing_compute_mask<SHIFT/2>(n);
+    n |= n >> SHIFT;
+    return n;
+}
 
-	_Mod_myrange_hashing() {}
-	_Mod_myrange_hashing(const std::__detail::_Mod_range_hashing &/*temp*/) {}
+// End recursion for compute_mask used in _Mask_range_hashing
+template<>
+inline std::size_t
+_Mask_range_hashing_compute_mask<1>(std::size_t n)
+{
+    n |= n >> 1;
+    return n;
+}
 
-	result_type operator()(first_argument_type __num, second_argument_type __den) const noexcept {
-		if (mask < __den) {
-			// Compute mask
-			// mask := (smallest power of two greater or equal than __den) - 1
-			std::size_t n = __den - 1;
-			n |= n >> 1;
-			n |= n >> 2;
-			n |= n >> 4;
-			n |= n >> 8;
-			n |= n >> 16;
-			n |= n >> 32;
-			mask = n;
-		}
-		const std::size_t probe = (__num & mask);
+// Specialization of compute_mask used in _Mask_range_hashing for 64bit systems
+template<>
+inline std::size_t
+_Mask_range_hashing_compute_mask<32>(std::size_t n)
+{
+    n |= n >> 1;
+    n |= n >> 2;
+    n |= n >> 4;
+    n |= n >> 8;
+    n |= n >> 16;
+    n |= n >> 32;
+    return n;
+}
+
+/// Range hashing function using masking to fold a large number into the
+/// range [0, N). We use a mask larger than the number of buckets. In case
+/// result of the masking is not a valid bucket number, we fall back to modulo
+/// as it is used in _Mod_range_hashing.
+struct _Mask_range_hashing
+{
+    typedef std::size_t first_argument_type;
+    typedef std::size_t second_argument_type;
+    typedef std::size_t result_type;
+
+    // mutable because it must be changed within operator()
+    mutable std::size_t _M_mask;
+
+    _Mask_range_hashing() : _M_mask(1) {}
+
+    result_type
+    operator()(first_argument_type __num,
+           second_argument_type __den) const noexcept
+    {
+        if (_M_mask < __den) {
+            _M_mask = _Mask_range_hashing_compute_mask<sizeof(std::size_t)*8/2>(__den - 1);
+        }
+        const std::size_t probe = (__num & _M_mask);
         // probe could be greater than the number of buckets since mask can be
         // greater. Do a modulo operation only if that is the case
-		const std::size_t b = (probe < __den) ? probe : (__num % __den);
+        const std::size_t b = (probe < __den) ? probe : (__num % __den);
 
-		return b;
-	}
+        return b;
+    }
 };
-
-std::size_t _Mod_myrange_hashing::mask = 1;
 
 namespace std {
 template <typename _Alloc, typename _ExtractKey, typename _Equal, typename _H1, typename _Hash, typename _RehashPolicy,
 		  typename _Traits>
 class _Hashtable<int, std::pair<const int, int>, _Alloc, _ExtractKey, _Equal, _H1, __detail::_Mod_range_hashing, _Hash,
 				 _RehashPolicy, _Traits>
-	: public _Hashtable<int, std::pair<const int, int>, _Alloc, _ExtractKey, _Equal, _H1, _Mod_myrange_hashing, _Hash,
+	: public _Hashtable<int, std::pair<const int, int>, _Alloc, _ExtractKey, _Equal, _H1, _Mask_range_hashing, _Hash,
 						_RehashPolicy, _Traits> {
   public:
-	using myBase = _Hashtable<int, std::pair<const int, int>, _Alloc, _ExtractKey, _Equal, _H1, _Mod_myrange_hashing,
+	using myBase = _Hashtable<int, std::pair<const int, int>, _Alloc, _ExtractKey, _Equal, _H1, _Mask_range_hashing,
 							  _Hash, _RehashPolicy, _Traits>;
 
 	using myBase::_Hashtable;
