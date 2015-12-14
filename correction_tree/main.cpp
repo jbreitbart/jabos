87db4e83edc9f8d733eb512559ce0c01790838d4
Jens Breitbart <jbreitbart@gmail.com>
2015-12-14 12:11:48 +0100

    Fixed correction tree test.

--- correction_tree/main.cpp
+++ correction_tree/main.cpp
@@ -8,10 +8,11 @@ extern "C" {
 using namespace std;
 
 int main(int argc, char *argv[]) {
-	cor_node *n1 = process_call(nullptr, nullptr);
-	cor_node *n2 = process_call(n1, nullptr);
-	cor_node *n3 = process_return(n2, nullptr);
-	cor_node *n4 = process_return(n3, nullptr);
+	jCC jccs[10];
+	cor_node *n1 = process_call(nullptr, &jccs[0]);
+	cor_node *n2 = process_call(n1, &jccs[1]);
+	cor_node *n3 = process_return(n2, &jccs[0]);
+	cor_node *n4 = process_return(n3, &jccs[2]);
 	assert(n3 == n1);
 	cout << n1->_cache_line_counter << endl;
 }
